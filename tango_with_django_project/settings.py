c42ac9f11381aa8b151c6a68f7995913ecab140f
ZihanXu1234 <2512237X@student.gla.ac.uk>
2021-07-28 23:45:23 +0100

    3 Test

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',   
+        'Name':str(os.path.join(BASE_DIR,"db.sqlite3"))
     }
 }
 
