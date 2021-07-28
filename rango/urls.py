4867d3e4ff10197a483879faa721ed62ed47a533
ZihanXu1234 <2512237X@student.gla.ac.uk>
2021-07-28 22:18:39 +0100

    3 Exercises

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,11 @@
+from django.http.response import HttpResponse
 from django.urls import path
 from rango import views
 app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
