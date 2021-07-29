b19dd52d0c4a0617447a64100d9caa3296f4a5cd
ZihanXu1234 <2512237X@student.gla.ac.uk>
2021-07-29 20:56:40 +0100

    5 Exercise

--- populate_rango.py
+++ populate_rango.py
@@ -12,28 +12,31 @@ def populate():
     # through each data structure, and add the data to our models.
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/'},
         {'title':'How to Think like a Computer Scientist',
-        'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
-        'url':'http://www.korokithakis.net/tutorials/python/'} ]
+         'url':'http://www.korokithakis.net/tutorials/python/'} ]
     django_pages = [
         {'title':'Official Django Tutorial',
-        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title':'Django Rocks',
-        'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django',
-        'url':'http://www.tangowithdjango.com/'} ]
+         'url':'http://www.tangowithdjango.com/'} ]
 
     other_pages = [
         {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'} ]
+         'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    # cats = {'Python': {'pages': python_pages},
+    #         'Django': {'pages': django_pages},
+    #         'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -41,7 +44,8 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        #c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -51,18 +55,21 @@ def populate():
             print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views=0):
-     p = Page.objects.get_or_create(category=cat, title=title)[0]
-     p.url=url
-     p.views=views
-     p.save()
-     return p
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
 
-def add_cat(name):
-     c = Category.objects.get_or_create(name=name)[0]
-     c.save()
-     return c
+#def add_cat(name):
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
 
 # Start execution here!
 if __name__ == '__main__':
-     print('Starting Rango population script...')
-     populate()
+    print('Starting Rango population script...')
+    populate()
